c3d6281be20847fa8f998493c35c19a1b39d1a4d
MuhammadAtiq757 <muhammadatiq757@gmail.com>
2023-12-04 23:45:47 +0600

    banner

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  
+  
+  RouterProvider,
+} from "react-router-dom";
+
+
+import { router } from './Rotes/Route';
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+ 
+
+   <div className='max-w-7xl mx-auto'>
+   <RouterProvider router={router} />
+    </div>
+  
+ 
+  </React.StrictMode>,
+)
